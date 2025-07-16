3fdedd3aadda41eec4b4434cf4e8f4b6a7272d00
Shivam Vikram Patil <udem1999@gmail.com>
2025-07-16 15:18:17 +0530

    Time: 0 ms (100.00%) | Memory: 29.4 MB (79.29%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -6,11 +6,9 @@ public:
             return;
         }
         k=k%n;
-        if(n>k){
         reverse(nums,0,n-k-1);
         reverse(nums,n-k,n-1);
         reverse(nums,0,n-1);
-        }
     }
 
     void reverse(vector<int>& nums, int low, int high){
