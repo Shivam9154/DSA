15dbd1e6392ae67dd3e1f1fc10cc6b3e069502bb
Shivam Vikram Patil <udem1999@gmail.com>
2025-07-19 17:31:26 +0530

    Time: 8 ms (51.40%) | Memory: 128.4 MB (57.61%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -5,12 +5,12 @@ public:
         vector<int>v(n,0);
         int pos=0, neg=1;
         for(int i=0;i<n;i++){
-            if(nums[i]<0){
-                v[neg]=nums[i];
-                neg+=2;
-            }else{
+            if(nums[i]>=0){
                 v[pos]=nums[i];
                 pos+=2;
+            }else{
+                v[neg]=nums[i];
+                neg+=2;
             }
         }
         return v;
