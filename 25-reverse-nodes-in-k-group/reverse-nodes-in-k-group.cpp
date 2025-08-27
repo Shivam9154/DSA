48256cf82f6d45ce11555af5db9c509e17befa22
Shivam Vikram Patil <udem1999@gmail.com>
2025-08-27 17:28:42 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (39.24%) - LeetSync

---
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -0,0 +1,64 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseLL(ListNode *head){
+        ListNode *curr=head;
+        ListNode *prev=NULL,*next=NULL;
+        while(curr!=NULL){
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next; 
+        }
+        return prev;
+    }
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode *dummyNode=new ListNode(-1);
+        dummyNode->next=head;
+        ListNode *p=dummyNode;
+        ListNode *tail=p->next;
+        ListNode *temp;
+        ListNode *tailPrev;
+        int count=0;
+        while(p->next!=NULL){
+            int i;
+            for(i=1;i<=k&&p->next;i++){
+                p=p->next;
+            }
+            if(i!=k+1){
+                break;
+            }
+            count++;
+            if(count==1){
+                ListNode *q=p->next;
+                p->next=NULL;
+                temp=reverseLL(tail);
+                head=temp;
+                p=tail;
+                tail->next=q;
+                tail=tail->next;
+                tailPrev=p;
+            }else{
+                ListNode *q=p->next;
+                p->next=NULL;
+                temp=reverseLL(tail);
+                p=tail;
+                tail->next=q;
+                tail=tail->next;
+                tailPrev->next=temp;
+                tailPrev=p;
+            }
+        }
+        delete dummyNode;
+        return head;
+    }
+};
