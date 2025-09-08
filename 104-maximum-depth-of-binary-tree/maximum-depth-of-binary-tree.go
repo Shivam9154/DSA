acd001929d7095c292e3c776a2cc5869af5e057a
Shivam Vikram Patil <udem1999@gmail.com>
2025-09-08 11:08:12 +0530

    Time: 0 ms (100.00%) | Memory: 6.3 MB (52.48%) - LeetSync

---
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.go
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func max(a,b int) int{
+    if(a>b){
+        return a
+    }
+    return b
+}
+func maxDepth(root *TreeNode) int {
+    if root==nil{
+        return 0;
+    }
+    if root.Left==nil && root.Right==nil{
+        return 1;
+    } 
+    return 1+max(maxDepth(root.Left),maxDepth(root.Right))
+}
